2954b419faa10c9c7baf5a6261abb45300b78675
Abdulloh Umar <iniautok@gmail.com>
2025-06-13 16:27:50 +0700

    first configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:chatting_app/firebase_options.dart';
 import 'package:chatting_app/provider/shared_preference_provider.dart';
 import 'package:chatting_app/screens/chat_screen.dart';
 import 'package:chatting_app/screens/login_screen.dart';
 import 'package:chatting_app/screens/register_screen.dart';
 import 'package:chatting_app/services/shared_preferences_service.dart';
 import 'package:chatting_app/static/screen_route.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -11,6 +13,10 @@ import 'package:shared_preferences/shared_preferences.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final pref = await SharedPreferences.getInstance();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
 
   runApp(MultiProvider(
     providers: [
@@ -28,7 +34,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
